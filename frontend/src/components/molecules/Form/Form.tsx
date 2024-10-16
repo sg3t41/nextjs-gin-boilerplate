39fe8609e0a5cc92f64d8626819bd828c4b49056
sg3t41 <sg3t41@gmail.com>
2024-10-16 20:08:51 +0900

    dev

--- frontend/src/components/molecules/Form/Form.tsx
+++ frontend/src/components/molecules/Form/Form.tsx
@@ -4,7 +4,15 @@ import { signUpAction } from '@/features/signup/actions/signUpAction'
 import { useFormState, useFormStatus } from 'react-dom'
 
 const initialState = {
-  message: '',
+  username: '',
+  email: '',
+  passwordHash: '',
+  errors: {
+    username: [],
+    email: [],
+    password: [],
+    commMessage: '',
+  },
 }
 
 function SubmitButton() {
@@ -21,12 +29,52 @@ export function Form() {
   const [state, formAction] = useFormState(signUpAction, initialState)
 
   return (
-    <form action={formAction}>
-      <label htmlFor='todo'>Enter Task</label>
-      <input type='text' id='todo' name='todo' required />
+    <form action={formAction} noValidate>
+      <label htmlFor='username'>Username</label>
+      <input type='text' id='username' name='username' required />
+      {state?.errors?.username && state.errors.username.length > 0 && (
+        <ul>
+          {state.errors.username.map((error, index) => (
+            <li key={index} className='error-message'>
+              {error}
+            </li>
+          ))}
+        </ul>
+      )}
+
+      <label htmlFor='email'>Email</label>
+      <input type='email' id='email' name='email' required />
+      {state?.errors?.email && state.errors.email.length > 0 && (
+        <ul>
+          {state.errors.email.map((error, index) => (
+            <li key={index} className='error-message'>
+              {error}
+            </li>
+          ))}
+        </ul>
+      )}
+
+      <label htmlFor='password'>Password</label>
+      <input type='password' id='password' name='password' required />
+      {state?.errors?.password && state.errors.password.length > 0 && (
+        <ul>
+          {state.errors.password.map((error, index) => (
+            <li key={index} className='error-message'>
+              {error}
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {state?.errors?.commMessage && state.errors.commMessage && (
+        <p className='error-message'>{state.errors.commMessage}</p>
+      )}
+
       <SubmitButton />
       <p aria-live='polite' className='sr-only' role='status'>
-        {state?.message}
+        {state?.username}
+        {state?.email}
+        {state?.passwordHash}
       </p>
     </form>
   )
--- frontend/src/features/signup/actions/signUpAction.ts
+++ frontend/src/features/signup/actions/signUpAction.ts
@@ -2,33 +2,82 @@
 
 import { revalidatePath } from 'next/cache'
 import { z } from 'zod'
+import * as utils from '@/utils/index'
+
+type State = {
+  username: string
+  email: string
+  passwordHash: string
+  errors?: {
+    username?: string[]
+    email?: string[]
+    password?: string[]
+    commMessage?: string
+  }
+}
 
 export async function signUpAction(
-  prevState: {
-    message: string
-  },
+  _: State | undefined,
   formData: FormData,
-) {
+): Promise<State | undefined> {
+  // validation scheme
   const schema = z.object({
-    todo: z.string().min(1),
-  })
-  const parse = schema.safeParse({
-    todo: formData.get('todo'),
+    username: z
+      .string({
+        invalid_type_error: 'ユーザー名が不正です。',
+      })
+      .max(20, { message: 'ユーザー名が長すぎます。' })
+      .refine(value => value.trim() !== '', {
+        message: 'ユーザー名を入力してください。',
+        path: ['username'],
+      }),
+    email: z
+      .string()
+      .email({ message: 'Invalid email format' })
+      .refine(value => value.trim() !== '', {
+        message: 'メールアドレスを入力してください。',
+        path: ['email'],
+      }),
+    password: z.string().refine(value => value.length >= 8, {
+      message: 'パスワードは8文字以上で入力してください。',
+      path: ['password'],
+    }),
   })
 
-  if (!parse.success) {
-    return { message: 'Failed to create todo' }
+  const validatedFields = schema.safeParse({
+    username: formData.get('username'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  if (!validatedFields.success) {
+    return {
+      username: '',
+      email: '',
+      passwordHash: '',
+      errors: validatedFields.error.flatten().fieldErrors,
+    }
   }
 
-  const data = parse.data
-
+  const { username, email, password } = validatedFields.data
   try {
-    console.log(data.todo)
+    console.log(username)
+    console.log(email)
+    console.log(password)
 
     revalidatePath('/')
-    return { message: `Added todo ${data.todo}` }
+    return {
+      username,
+      email,
+      passwordHash: utils.sha256.hash(password),
+    }
   } catch (e) {
     console.log(e)
-    return { message: 'Failed to create todo' }
+    return {
+      username: '',
+      email: '',
+      passwordHash: '',
+      errors: { commMessage: 'Failed to create todo' },
+    }
   }
 }
