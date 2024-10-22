244257d651ee201e869a1e985aa475dc22a680bd
sg3t41 <sg3t41@gmail.com>
2024-10-22 19:45:43 +0900

    dev

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -3,6 +3,7 @@ import { SignUpForm } from '@/features/signup/components/SignUpForm/SignUpForm'
 export default function SignUp() {
   return (
     <>
+      <h1>会員登録ページ</h1>
       <SignUpForm />
     </>
   )
